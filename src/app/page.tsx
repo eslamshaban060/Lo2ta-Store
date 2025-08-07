e437eaa51ca80ade0b95021867bc3bf9d7058da3
eslamshaban060 <eslamshaban060@gmail.com>
2025-08-07 03:27:51 +0200

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className=" text-7xl text-center w-[100vw] h-[100vh] ">
+      eslam shaban gomah
+    </div>
+  );
+};
+
+export default page;
